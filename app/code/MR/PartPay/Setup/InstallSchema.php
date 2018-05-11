542654524caa5322a77ebe2fe2e41407fd0966d3
Skyler Lan <bluezod@gmail.com>
2018-05-11 17:41:28 +1200

    Complete DB schema script

--- app/code/MR/PartPay/Setup/InstallSchema.php
+++ app/code/MR/PartPay/Setup/InstallSchema.php
@@ -10,25 +10,110 @@ use Magento\Framework\DB\Adapter\AdapterInterface;
 
 class InstallSchema implements InstallSchemaInterface
 {
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @throws \Zend_Db_Exception
+     */
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $installer = $setup;
 
         $installer->startSetup();
 
-        if (version_compare($context->getVersion(), '1.0.0') < 0){
+        $mrPartPayTableName = $installer->getTable('mr_partpay');
+        // if exists, then this module should be installed before, just skip it. Use upgrade command to updata the table.
+        if ($installer->getConnection()->isTableExists($mrPartPayTableName) != true) {
+            $mrPartPayTable = $installer->getConnection()
+                ->newTable($mrPartPayTableName)
+                ->addColumn(
+                    'id',
+                    Table::TYPE_INTEGER,
+                    11,
+                    [
+                        'identity' => true,
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'primary' => true
+                    ],
+                    'ID'
+                )
+                ->addColumn(
+                    'token',
+                    Table::TYPE_TEXT,
+                    255,
+                    'Token'
+                )
+                ->addColumn(
+                    'url',
+                    Table::TYPE_TEXT,
+                    255,
+                    'URL'
+                )
+                ->addColumn(
+                    'expire',
+                    Table::TYPE_TEXT,
+                    255,
+                    'Expire'
+                )
+                ->addColumn(
+                    'order_id',
+                    Table::TYPE_TEXT,
+                    255,
+                    'Order ID'
+                )
+                ->addColumn(
+                    'partpay_id',
+                    Table::TYPE_TEXT,
+                    255,
+                    'PartPay ID'
+                )
+                ->addIndex('token', 'token')
+                ->addIndex('order_id', 'order_id')
+                ->addIndex('partpay_id', 'partpay_id')
+                ->setComment('PartPay Orders Token Info etc.');
+            $installer->getConnection()->createTable($mrPartPayTable);
+        }
 
-		$installer->run('create table atesttable(atesttable_id int not null auto_increment, name varchar(100), primary key(atesttable_id))');
-$installer->run('insert into atesttable values(null,\'atesttable 1\')');
-$installer->run('insert into atesttable values(null,\'atesttable 2\')');
 
-
-		//demo 
-//$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-//$scopeConfig = $objectManager->create('Magento\Framework\App\Config\ScopeConfigInterface');
-//demo 
-
-		}
+        $configurationTableName = $installer->getTable('mr_partpay_configuration');
+        // if exists, then this module should be installed before, just skip it. Use upgrade command to updata the table.
+        if ($installer->getConnection()->isTableExists($configurationTableName) != true) {
+            $configurationTable = $installer->getConnection()
+                ->newTable($configurationTableName)
+                ->addColumn(
+                    'id',
+                    Table::TYPE_INTEGER,
+                    11,
+                    [
+                        'identity' => true,
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'primary' => true
+                    ],
+                    'ID'
+                )
+                ->addColumn(
+                    'store_id',
+                    Table::TYPE_INTEGER,
+                    11,
+                    'Store ID'
+                )
+                ->addColumn(
+                    'min',
+                    Table::TYPE_TEXT,
+                    255,
+                    'Min'
+                )
+                ->addColumn(
+                    'max',
+                    Table::TYPE_TEXT,
+                    255,
+                    'Max'
+                )
+                ->setComment('PartPay Configuration Table');
+            $installer->getConnection()->createTable($configurationTable);
+        }
 
         $installer->endSetup();
 
